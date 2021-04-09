404d2e2b8179b51e8389b1b29fed33f00377d40d
tmgasek <tomaszgasek28@gmail.com>
2021-04-09 10:26:16 +0100

    ready for heroku deployment phonebook

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', (req) => {
@@ -35,9 +37,13 @@ let persons = [
   },
 ];
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
+});
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/persons', (req, res) => {
